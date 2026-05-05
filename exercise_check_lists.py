a69b9d759c8c3f97cf3e732e4abe8a5b166b7772
Joaquinzanoni <jzanoni@mail.austral.edu.ar>
2026-05-04 22:01:37 -0300

    Add files via upload

--- exercise_check_lists.py
+++ exercise_check_lists.py
@@ -12,4 +12,11 @@ def check_lists(lista1, lista2):
     Returns:
         True si ambas listas tienen el mismo tercer elemento, False en caso contrario
     """
-    pass  # Reemplazar con tu implementación
+    if not len(lista1) >= 3 or not len(lista2) >=3:
+        return False
+    else:
+        if lista1[2] == lista2[2]:
+            return True
+        else:
+            return False
+        
